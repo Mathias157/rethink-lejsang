fe482187b7e5d67b0a52e7b82e39af33b4859c5b
Mathias157 <mathiasros@gmail.com>
2024-07-23 23:30:00 +0200

    Fixed unscalable posts

--- static/java.js
+++ static/java.js
@@ -1,14 +1,17 @@
 function show_post(number) {
     const top_container = document.querySelector(".top-container")
+    const post_container = document.querySelector(".post_container")
     const post = document.getElementById(`post${number}`)
     const header = document.querySelector(".post_header")
 
     
-    document.body.style.backgroundColor = "rgba(0, 0, 0, .15)"
+    document.body.style.backgroundColor = "var(--color1)"
     top_container.style.display =  'none'
+    post_container.style.display = 'flex'
     post.style.display = 'flex'
     
     header.addEventListener('click', function() {
+        post_container.style.display = 'none'
         post.style.display = 'none'
         document.body.style.backgroundColor = "white"
         top_container.style.display =  'block'
